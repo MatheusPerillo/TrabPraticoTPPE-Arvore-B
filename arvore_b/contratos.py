925b6fe1eb2d28285298299f79085f0137a9e9c3
Sabrina Berno <sabrinaberno@hotmail.com>
2025-06-23 10:44:35 -0300

    ajustes comentários

--- arvore_b/contratos.py
+++ arvore_b/contratos.py
@@ -7,45 +7,33 @@ if TYPE_CHECKING:
 # --- Pré-condições --- #
 
 def pre_inserir(tree: 'BTree', chave: int) -> bool:
-    """Pré-condição: A chave a ser inserida não pode existir na árvore."""
     return not tree.buscar(chave)
 
-
 def pre_remover(tree: 'BTree', chave: int) -> bool:
-    """Pré-condição: A chave a ser removida deve existir na árvore."""
     return tree.buscar(chave)
-
 
 # --- Pós-condições --- #
 
 def pos_remover(tree: 'BTree', chave: int) -> bool:
-    """Pós-condição: Após remover, a chave não deve mais existir na árvore."""
     return not tree.buscar(chave)
 
-
 def pos_raiz_chaves(tree: 'BTree') -> bool:
-    """Pós-condição: Para o nó raiz, número de chaves entre 1 e 2t-1."""
     raiz = tree.raiz
     return 1 <= len(raiz.chaves) <= 2 * tree.t - 1
 
-
 def pos_no_interno_chaves(tree: 'BTree') -> bool:
-    """Pós-condição: Para nós internos, número de chaves entre t-1 e 2t-1."""
     for no in tree._todos_nos():
         if no is not tree.raiz and not no.folha:
             if not (tree.t - 1 <= len(no.chaves) <= 2 * tree.t - 1):
                 return False
     return True
 
-
 def pos_raiz_filhos(tree: 'BTree') -> bool:
-    """Pós-condição: Para a raiz, número de filhos entre 2 e 2t."""
     raiz = tree.raiz
     return 2 <= len(raiz.filhos) <= 2 * tree.t
 
 
 def pos_no_interno_filhos(tree: 'BTree') -> bool:
-    """Pós-condição: Para nós internos, número de filhos entre t e 2t."""
     for no in tree._todos_nos():
         if no is not tree.raiz and not no.folha:
             if not (tree.t <= len(no.filhos) <= 2 * tree.t):
@@ -56,8 +44,6 @@ def pos_no_interno_filhos(tree: 'BTree') -> bool:
 # --- Invariantes Manuais --- #
 
 def verificar_invariantes(tree: 'BTree') -> None:
-    """Verifica os invariantes da Árvore-B."""
-
     assert all(no.chaves == sorted(no.chaves) for no in tree._todos_nos()), \
         "Erro: As chaves dos nós não estão ordenadas"
 
